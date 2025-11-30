c97e12d456901cf8fa5dbd7872d123e5f6250ff0
Angela <angela.xq.yang@gmail.com>
2025-11-30 04:17:13 -0800

    Adjusted window size - need to fix end screen and crane overlap

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let crane;
 let isPlaying = false;
 let bgSongNum;
 let bgSong;
-let cranes = [];
+//let cranes = [];
 
 function preload() {
   gif = loadImage('assets/happybirb.gif');
@@ -25,38 +25,30 @@ function preload() {
 
 /* SETUP RUNS ONCE */
 function setup() {
-  //createCanvas(windowWidth, windowHeight);
-  createCanvas(500, 500);
-  /*var s = 50;
-  var rows = floor(height/s);
-  var cols = floor(width/s);
-	
-  maze = new Maze(rows, cols, s);
-  maze.initialize();
-  maze.construct(0, 0, rows, cols);
-  frameRate(100);*/
+  createCanvas(windowWidth, windowHeight);
+  //createCanvas(500, 500);
 
-  //Randomly generates 10 coordinates for 10 cranes
-  x1 = random(1, 8333);
-  x2 = random(1, 8333);
-  x3 = random(1, 8333);
-  x4 = random(1, 8333);
-  x5 = random(1, 8333);
-  x6 = random(1, 8333);
-  x7 = random(1, 8333);
-  x8 = random(1, 8333);
-  x9 = random(1, 8333);
-  x10 = random(1, 8333);
-  y1 = random(1, 8333);
-  y2 = random(1, 8333);
-  y3 = random(1, 8333);
-  y4 = random(1, 8333);
-  y5 = random(1, 8333);
-  y6 = random(1, 8333);
-  y7 = random(1, 8333);
-  y8 = random(1, 8333);
-  y9 = random(1, 8333);
-  y10 = random(1, 8333);
+  // Randomly generates 10 coordinates for 10 cranes
+  x1 = random(1, windowWidth/0.1);
+  x2 = random(1, windowWidth/0.1);
+  x3 = random(1, windowWidth/0.1);
+  x4 = random(1, windowWidth/0.1);
+  x5 = random(1, windowWidth/0.1);
+  x6 = random(1, windowWidth/0.1);
+  x7 = random(1, windowWidth/0.1);
+  x8 = random(1, windowWidth/0.1);
+  x9 = random(1, windowWidth/0.1);
+  x10 = random(1, windowWidth/0.1);
+  y1 = random(1, windowHeight/0.1);
+  y2 = random(1, windowHeight/0.1);
+  y3 = random(1, windowHeight/0.1);
+  y4 = random(1, windowHeight/0.1);
+  y5 = random(1, windowHeight/0.1);
+  y6 = random(1, windowHeight/0.1);
+  y7 = random(1, windowHeight/0.1);
+  y8 = random(1, windowHeight/0.1);
+  y9 = random(1, windowHeight/0.1);
+  y10 = random(1, windowHeight/0.1);
 
   craneAni = loadAnimation(
     'assets/crane1.PNG',
@@ -103,11 +95,11 @@ function setup() {
   text("People say that if you manage to fold 1000 \norigami cranes, you are granted a singular \nwish. The objective of this game is to collect \n1000 cranes from this dark maze. Each crane will \nadd to the amount of light you have, and is \nequivalent to 100 paper cranes :)", width/2, height/2-70);
   setTimeout(draw, 1000);*/
 
-  light = createSprite(20, 20, 60);
+  light = createSprite(75, 75, 150);
   light.shape = "circle";
   light.color = "#dfe4ed";
   light.depth = 0;
-  player = createSprite(10, 20, 20, 20);
+  player = createSprite(10, 20, 50, 50);
   player.overlaps(light);
   player.depth = 1;
   player.addAni('side', 'assets/side1.png',
@@ -123,19 +115,19 @@ function setup() {
     'assets/back3.png',
     'assets/back4.png');
   player.addAni('idle', 'assets/idle.png');
-  //player.height = 10;
+  //player.height = 50;
 
-  for (let i = 0; i < 10; i++) {
+  /*for (let i = 0; i < 10; i++) {
     let crane = createSprite(0, 0);
     crane.addAni('fly', craneAni);
     crane.ani = 'fly';
     crane.rotation = 0;
     crane.rotationSpeed = 0;
     crane.depth = 10;
-    crane.scale = 0.06;
+    crane.scale = 0.1;
     crane.visible = false;
     cranes.push(crane);
-  }
+  }*/
 }
 
 function draw() {
@@ -164,20 +156,18 @@ function draw() {
   }
 
   strokeWeight(1);
-
-  //maze.display();
   textSize(12);
   text("Use your arrow keys to move \naround. It's pitch black right now, \nbut as you collect more cranes, \nthe amount of light you have will \nincrease :) Collect ten cranes!", width - 100, 60);
   text("Press space to play/stop bg music", width - 320, 30);
   text("People say, if you fold 1000 origami \ncranes, you will be granted one singular wish...", 150, height - 45);
 
-  //Display Score
+  // Display Score
   fill("#ffffff");
   textAlign(CENTER);
   textSize(20);
   text(score + " Origami Cranes", 395, 35);
 
-  //Makes it so the light moves dynamically with the player
+  // Makes it so the light moves dynamically with the player
   if (kb.pressing("left")) {
     if (light.x == player.x) {
       light.x -= light.d / 6;
@@ -243,176 +233,169 @@ function draw() {
     light.vel.y = 0;
   }
 
-  //Checks if there's a crane in the player's line of sight
-  if (Math.abs(light.x - x1 * 0.06) <= light.d / 2 && Math.abs(light.y - y1 * 0.06) <= light.d / 2) {
+  // Checks if there's a crane in the player's line of sight
+  if (Math.abs(light.x - x1 * 0.1) <= light.d / 2 && Math.abs(light.y - y1 * 0.1) <= light.d / 2) {
     push();
-    scale(0.06);
+    scale(0.1);
     animation(craneAni, x1, y1);
     pop();
 
-    if (Math.abs(light.x - x1 * 0.06) <= 10 && Math.abs(light.y - y1 * 0.06) <= 10) {
+    if (Math.abs(light.x - x1 * 0.1) <= 10 && Math.abs(light.y - y1 * 0.1) <= 10) {
       collect.play();
       score += 100;
-      light.d += 10;
+      light.d *= 1.1;
       x1 = -1000;
       y1 = -1000;
     }
   }
 
-  if (Math.abs(light.x - x2 * 0.06) <= light.d / 2 && Math.abs(light.y - y2 * 0.06) <= light.d / 2) {
+  if (Math.abs(light.x - x2 * 0.1) <= light.d / 2 && Math.abs(light.y - y2 * 0.1) <= light.d / 2) {
     push();
-    scale(0.06);
+    scale(0.1);
     animation(craneAni, x2, y2);
     pop();
 
-    if (Math.abs(light.x - x2 * 0.06) <= 10 && Math.abs(light.y - y2 * 0.06) <= 10) {
+    if (Math.abs(light.x - x2 * 0.1) <= 10 && Math.abs(light.y - y2 * 0.1) <= 10) {
       //if (!collect.isPlaying()) {
       collect.play();
       //}
       //collect.stop(2);
       score += 100;
-      light.d += 10;
+      light.d *= 1.1;
       x2 = -2000;
       y2 = -2000;
     }
   }
 
-  if (Math.abs(light.x - x3 * 0.06) <= light.d / 2 && Math.abs(light.y - y3 * 0.06) <= light.d / 2) {
+  if (Math.abs(light.x - x3 * 0.1) <= light.d / 2 && Math.abs(light.y - y3 * 0.1) <= light.d / 2) {
     push();
-    scale(0.06);
+    scale(0.1);
     animation(craneAni, x3, y3);
     pop();
 
-    if (Math.abs(light.x - x3 * 0.06) <= 10 && Math.abs(light.y - y3 * 0.06) <= 10) {
+    if (Math.abs(light.x - x3 * 0.1) <= 10 && Math.abs(light.y - y3 * 0.1) <= 10) {
       collect.play();
       score += 100;
-      light.d += 10;
+      light.d *= 1.1;
       x3 = -3000;
       y3 = -3000;
     }
   }
 
-  if (Math.abs(light.x - x4 * 0.06) <= light.d / 2 && Math.abs(light.y - y4 * 0.06) <= light.d / 2) {
+  if (Math.abs(light.x - x4 * 0.1) <= light.d / 2 && Math.abs(light.y - y4 * 0.1) <= light.d / 2) {
     push();
-    scale(0.06);
+    scale(0.1);
     animation(craneAni, x4, y4);
     pop();
 
-    if (Math.abs(light.x - x4 * 0.06) <= 10 && Math.abs(light.y - y4 * 0.06) <= 10) {
+    if (Math.abs(light.x - x4 * 0.1) <= 10 && Math.abs(light.y - y4 * 0.1) <= 10) {
       collect.play();
       score += 100;
-      light.d += 10;
+      light.d *= 1.1;
       x4 = -300;
       y4 = -300;
     }
   }
 
-  if (Math.abs(light.x - x5 * 0.06) <= light.d / 2 && Math.abs(light.y - y5 * 0.06) <= light.d / 2) {
+  if (Math.abs(light.x - x5 * 0.1) <= light.d / 2 && Math.abs(light.y - y5 * 0.1) <= light.d / 2) {
     push();
-    scale(0.06);
+    scale(0.1);
     animation(craneAni, x5, y5);
     pop();
 
-    if (Math.abs(light.x - x5 * 0.06) <= 10 && Math.abs(light.y - y5 * 0.06) <= 10) {
+    if (Math.abs(light.x - x5 * 0.1) <= 10 && Math.abs(light.y - y5 * 0.1) <= 10) {
       collect.play();
       score += 100;
-      light.d += 10;
+      light.d *= 1.1;
       x5 = -400;
       y5 = -400;
     }
   }
 
-  if (Math.abs(light.x - x6 * 0.06) <= light.d / 2 && Math.abs(light.y - y6 * 0.06) <= light.d / 2) {
+  if (Math.abs(light.x - x6 * 0.1) <= light.d / 2 && Math.abs(light.y - y6 * 0.1) <= light.d / 2) {
     push();
-    scale(0.06);
+    scale(0.1);
     animation(craneAni, x6, y6);
     pop();
     
-    if (Math.abs(light.x - x6 * 0.06) <= 10 && Math.abs(light.y - y6 * 0.06) <= 10) {
+    if (Math.abs(light.x - x6 * 0.1) <= 10 && Math.abs(light.y - y6 * 0.1) <= 10) {
       collect.play();
       score += 100;
-      light.d += 10;
+      light.d *= 1.1;
       x6 = -500;
       y6 = -500;
     }
   }
 
-  if (Math.abs(light.x - x7 * 0.06) <= light.d / 2 && Math.abs(light.y - y7 * 0.06) <= light.d / 2) {
+  if (Math.abs(light.x - x7 * 0.1) <= light.d / 2 && Math.abs(light.y - y7 * 0.1) <= light.d / 2) {
     push();
-    scale(0.06);
+    scale(0.1);
     animation(craneAni, x7, y7);
     pop();
 
-    if (Math.abs(light.x - x7 * 0.06) <= 10 && Math.abs(light.y - y7 * 0.06) <= 10) {
+    if (Math.abs(light.x - x7 * 0.1) <= 10 && Math.abs(light.y - y7 * 0.1) <= 10) {
       collect.play();
       score += 100;
-      light.d += 10;
+      light.d *= 1.1;
       x7 = -600;
       y7 = -600;
     }
   }
 
-  if (Math.abs(light.x - x8 * 0.06) <= light.d / 2 && Math.abs(light.y - y8 * 0.06) <= light.d / 2) {
+  if (Math.abs(light.x - x8 * 0.1) <= light.d / 2 && Math.abs(light.y - y8 * 0.1) <= light.d / 2) {
     push();
-    scale(0.06);
+    scale(0.1);
     animation(craneAni, x8, y8);
     pop();
 
-    if (Math.abs(light.x - x8 * 0.06) <= 10 && Math.abs(light.y - y8 * 0.06) <= 10) {
+    if (Math.abs(light.x - x8 * 0.1) <= 10 && Math.abs(light.y - y8 * 0.1) <= 10) {
       collect.play();
       score += 100;
-      light.d += 10;
+      light.d *= 1.1;
       x8 = -600;
       y8 = -600;
     }
   }
 
-  if (Math.abs(light.x - x9 * 0.06) <= light.d / 2 && Math.abs(light.y - y9 * 0.06) <= light.d / 2) {
+  if (Math.abs(light.x - x9 * 0.1) <= light.d / 2 && Math.abs(light.y - y9 * 0.1) <= light.d / 2) {
     push();
-    scale(0.06);
+    scale(0.1);
     animation(craneAni, x9, y9);
     pop();
 
-    if (Math.abs(light.x - x9 * 0.06) <= 10 && Math.abs(light.y - y9 * 0.06) <= 10) {
+    if (Math.abs(light.x - x9 * 0.1) <= 10 && Math.abs(light.y - y9 * 0.1) <= 10) {
       collect.play();
       score += 100;
-      light.d += 10;
+      light.d *= 1.1;
       x9 = -600;
       y9 = -600;
     }
   }
 
-  if (Math.abs(light.x - x10 * 0.06) <= light.d / 2 && Math.abs(light.y - y10 * 0.06) <= light.d / 2) {
+  if (Math.abs(light.x - x10 * 0.1) <= light.d / 2 && Math.abs(light.y - y10 * 0.1) <= light.d / 2) {
     push();
-    scale(0.06);
+    scale(0.1);
     animation(craneAni, x10, y10);
     pop();
     
-    if (Math.abs(light.x - x10 * 0.06) <= 10 && Math.abs(light.y - y10 * 0.06) <= 10) {
+    if (Math.abs(light.x - x10 * 0.1) <= 10 && Math.abs(light.y - y10 * 0.1) <= 10) {
       collect.play();
       score += 100;
-      light.d += 10;
+      light.d *= 1.1;
       x10 = -600;
       y10 = -600;
     }
   }
 
-  if (score == 1000) {
+  if (score == 100) {
     endScreen();
   }
 }
 
-/*
-function introScreen() {
-  
-}*/
-
 function endScreen() {
   light.visible = false;
   player.visible = false;
-  for (let crane of cranes) {
-    crane.visible = false;
-  }
+  
   background("#6e8fc4");
   //background("#dfe4ed");
   if (!isPlaying) {
@@ -422,18 +405,18 @@ function endScreen() {
   song.stop(2);
   textAlign(CENTER);
   //textColor("#1d1e26")
-  text("You winnnnn!!!!! \nGo ahead and make a wish. \nWho knows, maybe it will \ncome true someday :)", width / 2, height / 2 - 135);
+  text("You winnnnn!!!!! \nGo ahead and make a wish. \nWho knows, maybe it will \ncome true someday :)", width/2, height/2 - 135);
   
   push();
   scale(0.908);
-  image(gif, 27, height / 2 + 10);
+  image(gif, windowWidth/2, height/2 + 10);
   pop();
 
   push();
-  scale(0.06);
-  animation(craneAni, 8100, 700);
-  //scale(-1, 1);
+  scale(0.1);
   animation(craneAni, 850, 700);
+  animation(craneAni, width - 850, 700);
+  //scale(-1, 1);
   pop();
   //animation(craneAni, 4475, 700);
   //setTimeout(endScreen, 1000);
